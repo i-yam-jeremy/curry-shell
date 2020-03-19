118127f8144e35f032002459b7a9caab4ed92ae6
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2020-03-19 13:37:04 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
+std::string parseId(std::string source, int& pos) {
+  skipWhitespace(source, pos);
+  std::string id = "";
+  if (('a' <= source[pos] && source[pos] <= 'z') ||
+      ('A' <= source[pos] && source[pos] <= 'Z')) {
+    id += source[pos];
+    pos++;
+    while (('a' <= source[pos] && source[pos] <= 'z') ||
+           ('A' <= source[pos] && source[pos] <= 'Z') ||
+           ('0' <= source[pos] && source[pos] <= '9')) {
+      id += source[pos];
+      pos++;
+    }
+  }
+  return id;
+}
+
+ parseApplication(std::string source, int& pos) {
+  skipWhitespace(source, pos);
+  if (source == "0")
+}
+
 int main() {
   bool running = true;
   while (running) {
@@ -8,6 +30,11 @@ int main() {
     std::cin >> line;
     if (line == "exit") {
       running = false;
+    }
+    else {
+      int pos = 0;
+      std::string id = parseId(line, pos);
+      std::cout << "Id: " << id << std::endl;
     }
   }
   return 0;
